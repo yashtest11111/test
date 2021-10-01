eb37a43dbd5290f0ad62eb05f4d12a60b94f4078
yashs <yashsudani13@gmail.com>
2021-10-01 17:17:36 +0530

    test

---
+++ repo-info.py
@@ -0,0 +1,15 @@
+# importing the requests library
+import requests
+# defining the api-endpoint
+API_ENDPOINT = "http://1612-115-119-250-30.ngrok.io/train"
+# data to be sent to api
+data = {
+	"url": "https://github.com/yashtest11111/test.git",
+	"branch_name": "master",
+	"user_name": "yasd@af.com"
+}
+# sending post request and saving response as response object
+r = requests.post(url = API_ENDPOINT, data = data)
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
