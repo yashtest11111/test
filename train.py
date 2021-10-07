75c594267f30e0d37e79e21b144c34e4ca50b0c8
yashtest11111 <91733273+yashtest11111@users.noreply.github.com>
2021-10-07 20:23:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -10,6 +10,9 @@ from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
 
+tracking_uri = 'http://127.0.0.1:5000'
+mlflow.set_tracking_uri(tracking_uri)
+
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
