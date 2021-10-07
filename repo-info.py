04bf2f114d99afc2ab4c457935fa24e43a17e0ec
yashtest11111 <91733273+yashtest11111@users.noreply.github.com>
2021-10-07 15:10:19 +0530

    Update repo-info.py

--- repo-info.py
+++ repo-info.py
@@ -1,7 +1,7 @@
 # importing the requests library
 import requests
 # defining the api-endpoint
-API_ENDPOINT = "http://115.115.91.60:5432//train"
+API_ENDPOINT = "http://115.115.91.60:5432/train"
 # data to be sent to api
 data = {
 	"url": "https://github.com/yashtest11111/test.git",
